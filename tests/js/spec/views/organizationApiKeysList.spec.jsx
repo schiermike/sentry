5862b2404c3c71fb02bb04a04e8c6b2664dc50e6
Billy Vong <billy@sentry.io>
2018-04-25 21:59:58 -0700

    update tests

--- tests/js/spec/views/organizationApiKeysList.spec.jsx
+++ tests/js/spec/views/organizationApiKeysList.spec.jsx
@@ -1,14 +1,7 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import {mount} from 'enzyme';
 
 import OrganizationApiKeysList from 'app/views/settings/organization/apiKeys/organizationApiKeysList';
-
-const childContextTypes = {
-  organization: PropTypes.object,
-  router: PropTypes.object,
-  location: PropTypes.object,
-};
 
 const routes = [
   {path: '/'},
